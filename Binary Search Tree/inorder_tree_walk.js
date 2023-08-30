b23cd4aa38a28d5c6e6f2754476158de9bb911b9
Pallab Sonowal <pallabsonowal999@gmail.com>
2023-08-30 16:21:47 +0530

    solve inorder tree traversal by stack

--- Binary Search Tree/inorder_tree_walk.js
+++ Binary Search Tree/inorder_tree_walk.js
@@ -6,6 +6,34 @@ function inorderTreeWalkByRecursion(root) {
     inorderTreeWalkByRecursion(root.right);
 }
 
+function inorderTreeWalkByStack(root) {
+    if (root == null) return;
+
+    let stack = [root];
+    let isBacktracking = false;
+
+    while (stack.length != 0) {
+        let current = stack[stack.length - 1];
+
+        if (current == null) {
+            isBacktracking = true;
+            stack.pop();
+            continue;
+        }
+
+        if (!isBacktracking) {
+            stack.push(current.left);
+        } else {
+            console.log(current.element);
+            stack.pop();
+
+            stack.push(current.right);
+            isBacktracking = false;
+        }
+    }
+}
+
 module.exports = {
     inorderTreeWalkByRecursion,
+    inorderTreeWalkByStack
 }
--- Binary Search Tree/inorder_tree_walk.test.js
+++ Binary Search Tree/inorder_tree_walk.test.js
@@ -1,4 +1,4 @@
-const { inorderTreeWalkByRecursion } = require('./inorder_tree_walk');
+const { inorderTreeWalkByRecursion, inorderTreeWalkByStack } = require('./inorder_tree_walk');
 const { BinarySearchTree } = require('./BST');
 
 describe('Inorder Tree Walk', () => {
@@ -6,6 +6,7 @@ describe('Inorder Tree Walk', () => {
     let consoleLogSpy;
 
     beforeEach(() => {
+        bst = new BinarySearchTree();
         consoleLogSpy = jest.spyOn(console, 'log');
     })
 
@@ -15,18 +16,32 @@ describe('Inorder Tree Walk', () => {
 
     describe('By Recursion', () => {
         it('should print null', () => {
-            const bst = new BinarySearchTree();
-
             inorderTreeWalkByRecursion(bst.root);
         })
 
         it('should print out element using console', () => {
-            const bst = new BinarySearchTree();
             bst.add(2);
             bst.add(3);
             bst.add(1);
 
             inorderTreeWalkByRecursion(bst.root);
+            expect(consoleLogSpy).toHaveBeenNthCalledWith(1, 1);
+            expect(consoleLogSpy).toHaveBeenNthCalledWith(2, 2);
+            expect(consoleLogSpy).toHaveBeenNthCalledWith(3, 3);
+        })
+    })
+
+    describe('By Stack', () => {
+        it('should print null', () => {
+            inorderTreeWalkByStack(bst.root);
+        })
+
+        it('should print out element using console', () => {
+            bst.add(2);
+            bst.add(3);
+            bst.add(1);
+
+            inorderTreeWalkByStack(bst.root);
             expect(consoleLogSpy).toHaveBeenNthCalledWith(1, 1);
             expect(consoleLogSpy).toHaveBeenNthCalledWith(2, 2);
             expect(consoleLogSpy).toHaveBeenNthCalledWith(3, 3);
